29ec5eeb8f18fb4cf9583bb9207fb84ec9208c05
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2025-01-30 22:33:37 -0600

    Agregando NothingSelectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,10 @@
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography color="black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ab expedita enim ipsum possimus. Dolores explicabo, quidem aspernatur reiciendis adipisci distinctio. Laborum, cumque nisi nobis nam incidunt vitae odio reprehenderit!</Typography>
-
-      {/* NothingSelected */}
+      <NothingSelectedView/>
       {/* NoteView */}
       
     </JournalLayout>

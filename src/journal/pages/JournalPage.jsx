0c1529e18d31c52ade77b22237911a9d115946a9
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2025-01-30 21:40:40 -0600

    Agregando el NavBar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,14 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <Typography variant="h1">JournalPage</Typography>
+    <JournalLayout>
+      <Typography color="black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ab expedita enim ipsum possimus. Dolores explicabo, quidem aspernatur reiciendis adipisci distinctio. Laborum, cumque nisi nobis nam incidunt vitae odio reprehenderit!</Typography>
+
+      {/* NothingSelected */}
+      {/* NoteView */}
+      
+    </JournalLayout>
   )
 }

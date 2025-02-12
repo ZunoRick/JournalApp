eb3daeb036d10b5db74dfe4bf5b27e57012a7138
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2025-02-11 23:12:54 -0600

    Iniciando pruebas sobre JournalThunks

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,13 +6,24 @@ import { getFirestore } from 'firebase/firestore/lite'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// Dev/Prov
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCo5hb3DEWkWg5XzC374qUqHzXBSYv9OFQ",
+//   authDomain: "react-cursos-db244.firebaseapp.com",
+//   projectId: "react-cursos-db244",
+//   storageBucket: "react-cursos-db244.firebasestorage.app",
+//   messagingSenderId: "779637644101",
+//   appId: "1:779637644101:web:d713ec20436edd77a4fc2c"
+// };
+
+// Testing
 const firebaseConfig = {
-  apiKey: "AIzaSyCo5hb3DEWkWg5XzC374qUqHzXBSYv9OFQ",
-  authDomain: "react-cursos-db244.firebaseapp.com",
-  projectId: "react-cursos-db244",
-  storageBucket: "react-cursos-db244.firebasestorage.app",
-  messagingSenderId: "779637644101",
-  appId: "1:779637644101:web:d713ec20436edd77a4fc2c"
+  apiKey: "AIzaSyBHwVF8XFNjzETrOSg-8a-n-ELomDYA4QI",
+  authDomain: "tracker-4f4a2.firebaseapp.com",
+  projectId: "tracker-4f4a2",
+  storageBucket: "tracker-4f4a2.firebasestorage.app",
+  messagingSenderId: "855686563088",
+  appId: "1:855686563088:web:b7615afe4243f4bc919195"
 };
 
 // Initialize Firebase
